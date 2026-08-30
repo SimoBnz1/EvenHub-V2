51cb2f0f6cb412fd99f70da705d42c378c2d47da
mohamed_bnz <simobnz81@gmail.com>
2026-08-30 13:17:37 +0100

    feat: add traiteur reservation management

--- eventhub-frontend/src/routes/AppRoutes.jsx
+++ eventhub-frontend/src/routes/AppRoutes.jsx
@@ -8,21 +8,28 @@ import TraiteurDashboard from "../features/traiteur/pages/Dashboard";
 import CreateEvent from "../features/traiteur/pages/CreateEvent";
 import TraiteurEvents from "../features/traiteur/pages/Events";
 import EditEvent from "../features/traiteur/pages/EditEvent";
+import TraiteurReservations from "../features/traiteur/pages/Reservations";
+import ReservationCard from "../features/client/components/ReservationCard";
+
 function AppRoutes() {
     return (
-        
         <Routes>
+
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+
             <Route path="/events/:id" element={<EventDetails />} />
             <Route path="/reservation/:id" element={<Reservation />} />
-            <Route path="/traiteur/dashboard" element={<TraiteurDashboard/>}/>
+
+            <Route path="/traiteur/dashboard" element={<TraiteurDashboard />} />
             <Route path="/traiteur/events/create" element={<CreateEvent />} />
             <Route path="/traiteur/events" element={<TraiteurEvents />} />
             <Route path="/traiteur/events/:id/edit" element={<EditEvent />} />
-            
-            
+            <Route path="/traiteur/reservations" element={<TraiteurReservations />} />
+
+            <Route path="/reservations" element={<ReservationCard />} />
+
         </Routes>
     );
 }
